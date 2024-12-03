0d70c6886946e22a3d51d4df4e5d82474af78457
reykenj <230343s@mymail.nyp.edu.sg>
2024-12-03 16:32:38 +0800

    LAB 4 FUNCTIONS DONE

--- ICAFRAMEWORK/CLIENT/Lab1Exercise3/Exercise3.cpp
+++ ICAFRAMEWORK/CLIENT/Lab1Exercise3/Exercise3.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <winsock2.h>
 #include <conio.h>
+#include <string>
 
 /// Link with ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
@@ -16,44 +17,94 @@
 
 
 
-int packet_add_data(char Buffer[], const char DataName[], const int Value) {
-
-	char valueStr[20]; // Temporary buffer to hold the string representation of Value
-	int totalLength;
-
-	// Convert the integer value to a string
-	sprintf(valueStr, "%d", Value);
-
-	// Add DataName to the buffer
-	strncat(Buffer, DataName, sizeof(Buffer) );
-	strncat(Buffer, "=", sizeof(Buffer) );
-
-	// Add the value as a string to the buffer
-	strncat(Buffer, valueStr, sizeof(Buffer));
-
-	// Calculate the total length of the packet
-	totalLength = strlen(Buffer);
-
-	// Return the total length
-	return totalLength;
+int packet_add_data(char Buffer[], const char DataName[], const int Value)
+{
+	sprintf_s(Buffer, _msize(Buffer), "%s %s=%d", Buffer, DataName, Value);
+	return strlen(Buffer);
 }
-int packet_add_data(char Buffer[], const char DataName[], const char Value[]) {
 
-	int totalLength;
+int packet_add_data(char Buffer[], const char DataName[], const char Value[])
+{
+	sprintf_s(Buffer, _msize(Buffer), "%s %s=\"%s\"", Buffer, DataName, Value);
+	return strlen(Buffer);
+}
 
-	// Add DataName to the buffer
-	// - strlen(Buffer) - 1
-	strncat(Buffer, DataName, sizeof(Buffer));
-	strncat(Buffer, "=", sizeof(Buffer));
+int packet_parser_get_data(const char Packet[], const char DataName[], std::string& DataString)
+{
+	const char* Str;
+	if ((Str = strstr(Packet, DataName)) == NULL)
+	{
+		return 0;
+	}
+	const char* Pos = Str;
+	int Len = strlen(Pos);
+	for (int i = 0; i < Len; ++i)
+	{
+		if (Pos[i] == '=')
+		{
+			for (int j = i + 1; j < Len; ++j)
+			{
+				if (Pos[j] == ' ' || Pos[j] == '\n' || Pos[j] == '\0')
+				{
+					break;
+				}
+				DataString.push_back(Pos[j]);
+			}
+			return atoi(DataString.c_str());
+		}
+	}
 
-	// Add the value as a string to the buffer
-	strncat(Buffer, Value, sizeof(Buffer));
+	return 0;
+}
 
-	// Calculate the total length of the packet
-	totalLength = strlen(Buffer);
+int packet_parser_data(const char Packet[], const char DataName[])
+{
+	std::string DataString;
+	int ReturnLength;
 
-	// Return the total length
-	return totalLength;
+	return packet_parser_get_data(Packet, DataName, DataString);
+}
+
+int packet_parser_data(const char Packet[], const char DataName[], char Buffer[], int& BufferSize)
+{
+	std::string DataString;
+	int ReturnLength;
+	ReturnLength = packet_parser_get_data(Packet, DataName, DataString);
+	if (ReturnLength > BufferSize) {
+		ReturnLength = BufferSize;
+	}
+	strcpy_s(Buffer, BufferSize, DataString.c_str());
+	BufferSize = ReturnLength;
+	return ReturnLength;
+}
+
+int packet_encode(char Packet[], const int MaxBufferSize, const char PacketID[], const char PacketData[])
+{
+	int PacketLength = strlen(PacketID) + strlen(PacketData) + 7;
+	sprintf_s(Packet, _msize(Packet), "<%s %03d %s>", PacketID, PacketLength, PacketData);
+	return PacketLength;
+}
+
+int packet_decode(const char Packet[], char PacketID[], int& PacketIDLength, char PacketData[], int& PacketDataLength) {
+	if (Packet == NULL || PacketID == NULL || PacketData == NULL) {
+		return -1; // Error: invalid input
+	}
+	int PacketLength = strlen(Packet);
+	int Pos = 1;
+	int j = 0;
+	while (Pos < PacketLength && Packet[Pos] != ' ') {
+		PacketID[j++] = Packet[Pos++];
+	}
+
+	if (Pos >= PacketLength || Packet[Pos] != ' ') {
+		return -1;
+	}
+	PacketID[j] = '\0';
+	PacketIDLength = j;
+	++Pos;
+	strcpy(PacketData, &Packet[Pos]);
+	PacketDataLength = strlen(PacketData);
+	return strlen(Packet);
 }
 
 
@@ -78,7 +129,7 @@ int main(int argc, char** argv)
 
 	// PACKET TESTING 
 	char PacketBuffer[BUFSIZE] = { 0 }; // Initialize the buffer
-
+	packet_add_data(PacketBuffer, "ATK", 40);
 	packet_add_data(PacketBuffer, "DEF", 50);
 
 	// Print the resulting packet
